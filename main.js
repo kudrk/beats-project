4b809b0809a9ff5f8cc2b23fa09248421eb0620a
Kamila Kudryavtseva <kudrkv@gmail.com>
2020-09-17 11:29:01 +0300

    first commit of week 4

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
-// const hamburger = document.querySelector('.hamburger');
-// hamburger.addEventListener('click', function (e) {
-//   document.getElementById("menu").className = "menu_fullscreen";
-// })
-
-// const croce = document.querySelector ("#menu__croce")
-// croce.addEventListener('click', function (e) {
-//   document.getElementById("menu").menu.classList.toggle('menu_fullscreen');
-// })
+//hamburger menu
 
 const overlay = document.getElementById('menu');
 const closeMenu = document.getElementById('menu__croce');
 const hamburger = document.querySelector('.hamburger');
+const noscroll = document.querySelector('#no-scroll');
 
 
-document.getElementById('hamburger') .addEventListener('click', function() {
+document.getElementById('hamburger').addEventListener('click', function () {
   overlay.classList.add('menu_fullscreen');
+  document.getElementById('no-scroll');
+  noscroll.classList.add('no-scroll');
 });
 
-document.getElementById('menu__croce').addEventListener('click', function(){
-  overlay.classList.remove('menu_fullscreen')
+document.getElementById('menu__croce').addEventListener('click', function () {
+  overlay.classList.remove('menu_fullscreen');
+  document.getElementById('no-scroll');
+  noscroll.classList.remove('no-scroll');
 })
+
+// drop
+
+$(document).ready(() => {
+
+  $('.more').on('click', e => {
+    let drop = $(e.currentTarget).next();
+    drop.slideToggle();
+
+    const more = $('.more');
+
+    if (more.hasClass('active')) {
+      $(e.currentTarget).css({
+        'transform': 'rotate(0deg)'
+      });
+      $(e.currentTarget).removeClass('active');
+    } else {
+      $(e.currentTarget).css({
+        'transform': 'rotate(180deg)'
+      });
+      $(e.currentTarget).addClass('active');
+    }
+  });
+});
+
+// $(document).ready(() => {
+//   $('.crew-section__img').prependTo('.mini-drop');
+
+// });
