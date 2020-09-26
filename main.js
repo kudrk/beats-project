000e78c2dfeaba6f23300b59e5d8d15fe31dd287
Kamila Kudryavtseva <kudrkv@gmail.com>
2020-09-27 00:56:24 +0300

    map done

--- main.js
+++ main.js
@@ -396,7 +396,7 @@ const init = () => {
     myCollection = new ymaps.GeoObjectCollection({}, {
       draggable: false,
       iconLayout: 'default#image',
-      iconImageHref: './img/icons/marker.svg',
+      iconImageHref: './img/icons/marker.png',
       iconImageSize: [46, 57],
       iconImageOffset: [-35, -52]
     });
